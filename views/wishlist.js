92a29a0015df811009d89706ca26fbf51963aff0
Hemant Kumar <132387936+Hemant0621@users.noreply.github.com>
2024-02-04 23:30:48 +0530

    Add files via upload

---
+++ views/wishlist.js
@@ -0,0 +1,17 @@
+const product = require("../model/data");
+const mongoose = require("mongoose");
+
+
+
+
+module.exports={
+  
+  fetchData:function(req, res){
+    product.fetchData(async function(data){
+        
+      res.render('wishlist');
+      })
+    }
+}
+
+
