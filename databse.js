111cbbd2a46b5aaa33a4618d1bc3daa30e66f342
Hemant Kumar <132387936+Hemant0621@users.noreply.github.com>
2024-02-08 23:12:00 +0530

    Add files via upload

--- databse.js
+++ databse.js
@@ -15,7 +15,8 @@ app.set("view engine","ejs");
 
 app.use(require('./Routes/fetchdata'))
 
+const port = process.env.PORT || 3000;
 
-app.listen(3000,()=>{
-    console.log("listening on port 3000")
+app.listen(port,()=>{
+    console.log(`listening on port ${port}`)
 });
